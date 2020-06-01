6128681489b3e96a3306a08d96b264b5222a6a75
ibrahim <man.hema770@gmail.com>
2020-06-01 20:48:39 +0200

    task1

---
+++ task/src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author HEMA KHAFAGY
+ */
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        pro h1=new pro();
+        h1.setSize(960, 400);
+        h1.setResizable(false);
+        h1.setLocation(400, 150);
+        h1.setVisible(true);
+        
+    }
+    
+}
